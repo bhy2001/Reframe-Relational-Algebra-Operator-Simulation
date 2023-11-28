917507772182540c3a47506fddca63eb60355e2a
rohdma02 <rohdma02@luther.edu>
2023-11-27 23:38:32 -0600

    Changed the init and implemented Project

--- reframe.py
+++ reframe.py
@@ -4,59 +4,82 @@ warnings.filterwarnings('ignore')
 
 __all__ = ['Relation', 'GroupWrap']
 
+
 class Relation():
-    
-    def __init__(self,filename:str):
-        #sef.data = dict(col_heads)
-        #col_heads = [records]
-        self.data = {}
-        with open(filename) as csvFile:
-            col_head = next(csvFile).replace("\n", "").split(",")
-            for i in col_head:   
-                self.data[i] = []
-            read_file = csv.reader(csvFile)
-            for row in read_file:
-                for index, val in enumerate(row):
-                    self.data.get(col_head[index]).append(val)
+
+    def __init__(self, data):
+        if isinstance(data, str):
+            self.data = {}
+            with open(data) as csvFile:
+                col_head = next(csvFile).replace("\n", "").split(",")
+                for i in col_head:
+                    self.data[i] = []
+                read_file = csv.reader(csvFile)
+                for row in read_file:
+                    for index, val in enumerate(row):
+                        self.data.get(col_head[index]).append(val)
+        elif isinstance(data, dict):
+            self.data = data
+
     # Singe-table operations:
 
     #     project(cols)
-    def project (self,cols):
-        pass
+    def project(self, cols):
+        new_data = {col: self.data[col] for col in cols if col in self.data}
+        return Relation(new_data)
     #     rename(old, new)
-    def rename (self,old,new):
+
+    def rename(self, old, new):
         pass
     #     extend(name, formula)
+
     def extend(self, name, formula):
         pass
     #     select(query)
+
     def select(self, query):
         pass
     #     sort(cols, order)
+
     def sort(self, cols, order):
         pass
     #     gropby(cols)
+
     def groupby(self, cols):
         pass
     # Multi-table operations:
 
     #     product(other)
-    def product (self, other):
+    def product(self, other):
         pass
     #     union(other)
+
     def union(self, other):
         pass
     #     join(other, condition)
-    def join (self, other, condition):
+
+    def join(self, other, condition):
         pass
     #     semijoin(other)
+
     def semijoin(self, other):
         pass
     #     antijoin(other)
+
     def antijoin(self, other):
         pass
     #     outerjoin(other)
-    def outerjoin (self, other):
+
+    def outerjoin(self, other):
         pass
 
-Dept = Relation('./college/COURSE.csv')
+
+Courses = Relation('./college/COURSE.csv')
+Dept = Relation('./college/DEPT.csv')
+
+# TESTING FOR PROJECT METHOD
+resultCourses = Courses.project(['CId', 'Title'])
+print(resultCourses.data)
+print("================================================")
+resultDept = Dept.project(['DId', 'DName'])
+print(resultDept.data)
