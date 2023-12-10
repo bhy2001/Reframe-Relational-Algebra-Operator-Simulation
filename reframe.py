e91204c120d228e513f63c7b8e8e52ac2ba60a69
rohdma02 <rohdma02@luther.edu>
2023-12-10 14:44:23 -0600

    Finish the join operator

--- reframe.py
+++ reframe.py
@@ -5,8 +5,8 @@ warnings.filterwarnings('ignore')
 __all__ = ['Relation', 'GroupWrap']
 
 # Mat:
-# - Project
-# - Rename
+# - Project - handle error
+# - Rename - hanndle error
 
 # Bill:
 # - Select
@@ -82,7 +82,30 @@ class Relation():
 
     #     join(other, condition)
     def join(self, other, condition):
-        pass
+        # Condition should be a tuple specifying (left_col, right_col)
+        left_col, right_col = condition
+
+        # Construct new dictionary to hold join output
+        join_dict = {}
+        for col in set(self.filename.keys()) | set(other.filename.keys()):
+            join_dict[col] = []
+
+        # Go through rows and populate output
+        for i in range(len(self.filename[left_col])):
+            left_val = self.filename[left_col][i]
+
+            # Find matching right val
+            for j in range(len(other.filename[right_col])):
+                right_val = other.filename[right_col][j]
+                if left_val == right_val:
+                    # Match found, populate output row
+                    for col in join_dict:
+                        if col in self.filename:
+                            join_dict[col].append(self.filename[col][i])
+                        elif col in other.filename:
+                            join_dict[col].append(other.filename[col][j])
+
+        return Relation(join_dict)
     #     semijoin(other)
 
     def semijoin(self, other):
@@ -95,3 +118,62 @@ class Relation():
 
     def outerjoin(self, other):
         pass
+
+
+# Courses table
+# CId,Title,DeptId
+# 12,db systems,10
+# 22,compilers,10
+# 32,calculus,20
+# 42,algebra,20
+# 52,acting,30
+# 62,elocution,30
+
+
+# Enroll table
+# EId,StudentId,SectionId,Grade
+# 14,1,13,A
+# 24,1,43,C
+# 34,2,43,B+
+# 44,4,33,B
+# 54,4,53,A
+# 64,6,53,A
+
+
+# Dept table
+# DId,DName
+# 10,compsci
+# 20,math
+# 30,drama
+
+
+# Section table
+# SectId,CourseId,Prof,YearOffered
+# 13,12,turing,2018
+# 23,12,turing,2016
+# 33,32,newton,2017
+# 43,32,einstein,2018
+# 53,62,brando,2017
+# 13,12,turing,1988
+# 33,32,newton,2017
+
+
+# Student table
+# SId,SName,GradYear,MajorId
+# 1,joe,2021,10
+# 2,amy,2020,20
+# 3,max,2022,10
+# 4,sue,2022,20
+# 5,bob,2020,30
+# 6,kim,2020,20
+# 7,art,2021,30
+# 8,pat,2019,20
+# 9,lee,2021,10
+
+Courses = Relation('./college/COURSE.csv')
+Enroll = Relation('./college/ENROLL.csv')
+Section = Relation('./college/SECTION.csv')
+Student = Relation('./college/STUDENT.csv')
+Dept = Relation('./college/Dept.csv')
+
+
--- test.py
+++ test.py
@@ -1,7 +1,60 @@
 from reframe import Relation
 
 Courses = Relation('./college/COURSE.csv')
-Dept = Relation('./college/DEPT.csv')
+"""
+CId,Title,DeptId
+12,db systems,10
+22,compilers,10
+32,calculus,20
+42,algebra,20
+52,acting,30
+62,elocution,30
+"""
+Enroll = Relation('./college/ENROLL.csv')
+"""
+EId,StudentId,SectionId,Grade
+14,1,13,A
+24,1,43,C
+34,2,43,B+
+44,4,33,B
+54,4,53,A
+64,6,53,A
+
+"""
+Section = Relation('./college/SECTION.csv')
+"""
+SectId,CourseId,Prof,YearOffered
+13,12,turing,2018
+23,12,turing,2016
+33,32,newton,2017
+43,32,einstein,2018
+53,62,brando,2017
+13,12,turing,1988
+33,32,newton,2017
+
+"""
+Student = Relation('./college/STUDENT.csv')
+"""
+SId,SName,GradYear,MajorId
+1,joe,2021,10
+2,amy,2020,20
+3,max,2022,10
+4,sue,2022,20
+5,bob,2020,30
+6,kim,2020,20
+7,art,2021,30
+8,pat,2019,20
+9,lee,2021,10
+
+"""
+Dept = Relation('./college/Dept.csv')
+"""
+DId,DName
+10,compsci
+20,math
+30,drama
+
+"""
 
 # TESTING FOR PROJECT METHOD
 resultCourses = Courses.project(['CId', 'Title'])
@@ -23,6 +76,13 @@ print("================================================")
 
 
 # TESTING FOR UNION METHOD
-resultUnion = Courses.union(Dept)
-print("Union Result:")
-print(resultUnion.filename)
+# resultUnion = Courses.union(Dept)
+# print("Union Result:")
+# print(resultUnion.filename)
+
+
+# TESTING FOR JOIN METHOD
+join_cond = ('DeptId', 'DId')
+result = Courses.join(Dept, join_cond)
+print(result.filename)
+print("================================================")
